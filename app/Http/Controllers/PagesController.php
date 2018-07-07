a8841459e2e8b11882efc94cd88633b30af66037
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-07-07 12:53:21 +0200

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function contact() {
+      return view('pages.contact');
+    }
+    public function about() {
+      return view('pages.about');
+    }
+}
